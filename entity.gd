fc91a85548a802f7e6740b0a7eb4672da46df43b
CaptainPDA <chriscortez1994@gmail.com>
2019-12-29 20:16:29 -0600

    Add files via upload

---
+++ entity.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1) ##Defining what Direction is up
+const GRAVITY = 40
+const ACCELERATION = 50
+const MAX_SPEED = 400
+const JUMP_HEIGHT = -800
+
+var motion = Vector2()
+
